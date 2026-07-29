57d458cc0babd4082bcb7dcbff03a445e95a4ef1
zeph <zephyrzefa15@gmail.com>
2026-07-30 00:00:19 +0200

    ci(v1.0.0): pretty release tag

--- scripts/build_firmware.sh
+++ scripts/build_firmware.sh
@@ -14,20 +14,18 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Use avr-objcopy to convert the ELF file to a HEX file
+# Use avr-objcopy to convert the ELF file to BIN and HEX files
 if [ "$1" = "--release" ]; then
-    avr-objcopy -O binary -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/avr-none/release/rf64.elf target/rf64.bin
-    avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/avr-none/release/rf64.elf target/rf64.hex
+    cp target/avr-none/release/rf64.elf target/rf64.elf
+    avr-objcopy -O binary -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/rf64.elf target/rf64.bin
+    avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/rf64.elf target/rf64.hex
 else
-    avr-objcopy -O binary -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/avr-none/debug/rf64.elf target/rf64.bin
-    avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/avr-none/debug/rf64.elf target/rf64.hex
+    cp target/avr-none/debug/rf64.elf target/rf64.elf
+    avr-objcopy -O binary -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/rf64.elf target/rf64.bin
+    avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures -R .noinit -R .bss target/rf64.elf target/rf64.hex
 fi
 
 echo "Build successful. Output files:"
-if [ "$1" = "--release" ]; then
-    echo " - target/rf64.bin"
-    echo " - target/rf64.hex"
-else
-    echo " - target/rf64.bin"
-    echo " - target/rf64.hex"
-fi
+echo " - target/rf64.elf"
+echo " - target/rf64.bin"
+echo " - target/rf64.hex"
